9159ad73856b9975c0422a9e0676630128a58129
Risto Pejasinovic <risto.pejasinovic@cern.ch>
2022-11-16 00:01:53 +0100

    fix gcc_compiler

--- cmake/sc_uvm_env_setup.cmake
+++ cmake/sc_uvm_env_setup.cmake
@@ -1,4 +1,7 @@
 # set(CMAKE_FIND_DEBUG_MODE True)
+find_file(gcc_conf_cmake "gcc_conf.cmake"
+    PATHS "$ENV{SC_UVM_ENV_HOME}/osci" NO_DEFAULT_PATH)
+include(${gcc_conf_cmake})
 find_file(verilator_cmake "verilator.cmake"
     PATHS "$ENV{SC_UVM_ENV_HOME}/*" NO_DEFAULT_PATH)
 include(${verilator_cmake})
