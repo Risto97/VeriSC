742dbb84367bbd25eaf8b0a16560cb756f8002df
Risto Pejasinovic <risto.pejasinovic@cern.ch>
2023-02-14 22:57:05 +0100

    add RDL_FILES to recursive flatten

--- cmake/flatten_rtl_lib.cmake
+++ cmake/flatten_rtl_lib.cmake
@@ -27,7 +27,7 @@ function(flatten_rtl_lib OUT_LIB)
         target_include_directories(${OUT_LIB} INTERFACE ${inc_dirs})
     endforeach()
 
-    set(PROPERTY_APPEND_LIST "VERILATOR_CFG_FILES;VERILATOR_ARGS;VERILOG_DEFS;VCS_ARGS")
+    set(PROPERTY_APPEND_LIST "VERILATOR_CFG_FILES;VERILATOR_ARGS;VERILOG_DEFS;VCS_ARGS;RDL_FILES") # APPEND HERE from ARGV TODO
     foreach(lib ${LINK_LIBS})
         foreach(property_name ${PROPERTY_APPEND_LIST})
             safe_get_target_property(property_val ${lib} ${property_name} "")
