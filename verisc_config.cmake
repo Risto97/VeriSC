32453d03f28d40b8ef829f70f7dad8c81f530e22
Risto Pejasinovic <risto.pejasinovic@gmail.com>
2023-05-14 20:28:01 +0200

    allow -D<DEP>_VERSION=<VERSION> to be passed from cmake

--- verisc_config.cmake
+++ verisc_config.cmake
@@ -4,18 +4,32 @@
 # VERISC_CFG file will then be included after this one, and variables will be overwritten
 # You can set VCS_HOME in the VERISC_CFG file also, or provide it with cmake -DVCS_HOME=
 
-set(CMAKE_CXX_STANDARD 17)
+if(NOT CMAKE_CXX_STANDARD)
+    set(CMAKE_CXX_STANDARD 17)
+endif()
 
 # Select version of dependencies you want to be built
 # If in addition to version <DEP>_HOME is provided pointing to the root directory of the library or tool,
 # then dependency will not be compiled, instead the one provided below will be used, 
-set(SYSTEMC_VERSION 2.3.3)
+if(NOT SYSTEMC_VERSION)
+    set(SYSTEMC_VERSION 2.3.3)
+endif()
 # set(SYSTEMC_HOME <path-to-systemc>)
-set(SCV_VERSION 2.0.1)
-set(UVM-SYSTEMC_VERSION 1.0-beta4)
-set(VERILATOR_VERSION v5.006)
-set(FC4SC_VERSION master)
-set(ICSC_COMPILER_VERSION master)
+if(NOT SCV_VERSION)
+    set(SCV_VERSION 2.0.1)
+endif()
+if(NOT UVM-SYSTEMC_VERSION)
+    set(UVM-SYSTEMC_VERSION 1.0-beta4)
+endif()
+if(NOT VERILATOR_VERSION)
+    set(VERILATOR_VERSION v5.006)
+endif()
+if(NOT FC4SC_VERSION)
+    set(FC4SC_VERSION master)
+endif()
+if(NOT ICSC_COMPILER_VERSION)
+    set(ICSC_COMPILER_VERSION master)
+endif()
 
 # if you want to disable a dependency from being used set variable set(<DEP>_VERSION FALSE)
 # set(ICSC_COMPILER_VERSION FALSE)
@@ -25,6 +39,8 @@ set(ICSC_COMPILER_VERSION master)
 
 # VeriSC will try to use GCC from the system if it satisfies the CMAKE_CXX_STANDARD
 # If not it will compile the following version, otherwise GCC_VERSION is ignored
-set(GCC_VERSION 12.2.0)
+if(NOT GCC_VERSION)
+    set(GCC_VERSION 12.2.0)
+endif()
 # Example to use preinstalled GCC
 # set(GCC_HOME /mnt/ext/verisc/gcc-12.2.0/)
